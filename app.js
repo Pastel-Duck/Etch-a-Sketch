fd20d1306ac0cd5e14f7ddde2c5c7e6e53b73dfa
Oussama <pastel.lequack@gmail.com>
2024-09-05 15:55:55 +0100

    Add grid size function

--- app.js
+++ app.js
@@ -37,3 +37,45 @@ function coloringGrid(){
         )
     });
 }
+
+function gridSize(){
+    let btn = document.querySelector('.btn');
+    btn.addEventListener('click', (e) => {
+        let grid = prompt("Enter a number from 16 to 64");
+        let container = document.querySelector('#container');
+        let bigDiv = document.getElementsByClassName('big');
+        
+        function removeAllChildNodes(){
+            while(container.firstChild){
+                container.removeChild(container.firstChild);
+            }
+            while(bigDiv.firstChild){
+                bigDiv.removeChild(bigDiv.firstChild);
+            }
+        }
+            if(grid < 16 || grid > 64 || isNaN(grid) == true){
+                removeAllChildNodes();
+                startingGrid();
+            }
+            else{
+                removeAllChildNodes();
+                for(let i = 0; i < grid; i++){
+                    let divBig = document.createElement("div");
+                    divBig.className = "big";
+                    container.appendChild(divBig);
+            
+                    let j = 0;
+                    do{
+                        let divSmall = document.createElement("div");
+                        divSmall.className = "small";
+                        divBig.appendChild(divSmall);
+                        j++
+                    }
+                    while(j < grid);
+                }
+                coloringGrid();
+            }
+        })
+    }
+
+    gridSize();
